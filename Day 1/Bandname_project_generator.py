294861200d351f44b6aa84714374e5e554e78184
Precious Inegbenose <78587294+preshayo99@users.noreply.github.com>
2025-10-01 11:34:18 +0100

    initial commit

--- Day 1/Bandname_project_generator.py
+++
@@ -1,6 +0,0 @@
-print('Hello user! This is a band name generator program')
-run= input('Press enter to continue')
-
-city = input('Which city did you grow up in?:\n')
-pet = input('What is the name of your pet?: \n')
-print('Your band name could be: ' + city + ' ' + pet)
